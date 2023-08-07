f1b1c57e7203fae8289bfced6042bd364e2b1d63
Justina <justinamary27@gmail.com>
2023-08-07 22:23:45 +0530

    updated slack oauth token

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
-export const SLACK_OAUTH_TOKEN = 'xoxb-5481223687892-5694838013542-kSax2LTpfMnugiYnb6KBKdjJ'
+export const SLACK_OAUTH_TOKEN = 'xoxb-5481223687892-5694838013542-dmYvUl087pGLrYZEwM0aNsEO'
 export const BTC_SIGNAL = '#btc-signal' // this is the channel you want your bot online & spam to go
